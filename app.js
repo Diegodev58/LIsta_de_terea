cf08d5788d1cdcdbc3b21c4f38c55eb92636707a
Diego <diegoborregales7@gmail.com>
2025-03-04 12:19:53 -0400

    Avance de la app

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 
+const {MenuUsuario} = require('./inquirer/inquirerAj')
+const {pausa} = require('./inquirer/inquirerAj')
 
+
+console.clear()
 const main = async() => {
-    console.log("hola mundo");
+
+    let opt = '';
+    do {
+        opt = await MenuUsuario();
+        await pausa();
+    } while (opt !== '0');
+
+   
 }
 
 main();
---
+++ inquirer/preguntas.js
@@ -0,0 +1,9 @@
+const preguntas = [
+    {
+    type:'list',
+    name: 'opcion',
+    message: '¿Que desea hacer?',
+    choices: ['1 opt', '2 opt', '3 opt']
+}]
+
+module.exports = preguntas
